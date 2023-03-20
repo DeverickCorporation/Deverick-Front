34aac95007691b34a6e7de2ce167cdaf91d3cac3
eralinkd <andreyderkach2005@gmail.com>
2023-03-21 01:44:13 +0200

    almost done mobile version

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -27,17 +27,19 @@ function PostItem(props) {
 
     return (
         <div className='post'>
-            <h2>{post_json.post_title}<span> ({post_json.author_name})</span></h2>
-            <div className='heart-container'>
-                <span>{post_json.likes_num + (post_json.user_like ? (like ? 0 : -1) : (like ? 1 : 0))}</span>
-                {like ?
-                    <svg onClick={handleSetLike} className='liked' width="30" height="26" viewBox="0 0 30 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M15 6.12245L17.4904 3.49656C19.4068 1.47583 22.4994 1.12741 24.8174 2.67109C27.6066 4.52853 28.211 8.37385 26.1263 10.9975L15 25L3.87367 10.9975C1.78895 8.37385 2.39345 4.52853 5.1826 2.67109C7.50061 1.12741 10.5932 1.47583 12.5096 3.49656L15 6.12245Z" stroke="black" />
-                    </svg>
-                    :
-                    <svg onClick={handleSetLike} className='' width="30" height="26" viewBox="0 0 30 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M15 6.12245L17.4904 3.49656C19.4068 1.47583 22.4994 1.12741 24.8174 2.67109C27.6066 4.52853 28.211 8.37385 26.1263 10.9975L15 25L3.87367 10.9975C1.78895 8.37385 2.39345 4.52853 5.1826 2.67109C7.50061 1.12741 10.5932 1.47583 12.5096 3.49656L15 6.12245Z" stroke="black" />
-                    </svg>}
+            <div className='title-heart'>
+                <h2>{post_json.post_title}<span> ({post_json.author_name})</span></h2>
+                <div className='heart-container'>
+                    <span>{post_json.likes_num + (post_json.user_like ? (like ? 0 : -1) : (like ? 1 : 0))}</span>
+                    {like ?
+                        <svg onClick={handleSetLike} className='liked' width="30" height="26" viewBox="0 0 30 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15 6.12245L17.4904 3.49656C19.4068 1.47583 22.4994 1.12741 24.8174 2.67109C27.6066 4.52853 28.211 8.37385 26.1263 10.9975L15 25L3.87367 10.9975C1.78895 8.37385 2.39345 4.52853 5.1826 2.67109C7.50061 1.12741 10.5932 1.47583 12.5096 3.49656L15 6.12245Z" stroke="black" />
+                        </svg>
+                        :
+                        <svg onClick={handleSetLike} className='' width="30" height="26" viewBox="0 0 30 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15 6.12245L17.4904 3.49656C19.4068 1.47583 22.4994 1.12741 24.8174 2.67109C27.6066 4.52853 28.211 8.37385 26.1263 10.9975L15 25L3.87367 10.9975C1.78895 8.37385 2.39345 4.52853 5.1826 2.67109C7.50061 1.12741 10.5932 1.47583 12.5096 3.49656L15 6.12245Z" stroke="black" />
+                        </svg>}
+                </div>
             </div>
             <p className='text'>{post_json.post_text}</p>
             <p className='data'>{post_json.time}</p>
